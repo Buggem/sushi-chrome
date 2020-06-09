94ffdd7ce98f71743f97ca8291470ca29362b011
BuggemDeveloper <66670928+BuggemDeveloper@users.noreply.github.com>
2020-06-09 19:27:59 +1000

    Add files via upload

---
+++ Sushi/scripts/normal.js
@@ -0,0 +1,15 @@
+// JavaScript Sushi
+// Sushi talking
+alert("Hiya dude");
+alert("My name's Sushi, what's yours?");
+var name = prompt("Enter your name here:");
+var greeting = "Hey " + name + "! Do you want to learn Sushi?";
+alert(greeting);
+var q = prompt("Say yes or no:");
+var a = "Ok, the answer was " + q + ". But either way, lets at least teach you the basics. (Even if the answer is no!)";
+alert(a);
+alert("I will give you a tutorial, soon, but we are making one right now. Come back to check later! (My memory isn't very good, so you'll have to enter your name again)");
+alert("Oh, and one last thing. " + name + ", if you already know how to how to code Sushi, click on the Sushi Coding Alias on your hard drive in the /Sushi/ directory. It will run after I finish talking.");
+// Sushi talking end
+// Code starts here
+// Code ends here
